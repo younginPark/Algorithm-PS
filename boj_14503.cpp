c5a8b57af8b7482389a10c566974ca625c3d85f2
younginPark <youngin.park9@gmai.com>
2021-02-12 16:50:26 +0900

    2020.02.12

---
+++ boj_14503.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int dy[4] = { -1, 0, 1, 0}; //북동남서
+int dx[4] = { 0, 1, 0, -1};
+int N, M;
+int arr[51][51];
+int answer;
+
+struct ROBOT{
+    int y, x, dir;
+};
+
+queue<ROBOT> q;
+
+int main(){
+    ROBOT robot;
+    cin >> N >> M;
+    cin >> robot.y >> robot.x >> robot.dir;
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < M; j++){
+            cin >> arr[i][j];
+        }
+    }
+    q.push(robot);
+    while(!q.empty()){
+        ROBOT cur = q.front();
+        q.pop();
+        if(arr[cur.y][cur.x] == 0){
+            arr[cur.y][cur.x] = 2;
+            answer++;
+        }
+        for(int i = 0; i < 4; i++){
+            ROBOT next;
+            next.dir = (cur.dir + 3 + i*3) % 4;
+            next.y = cur.y + dy[next.dir];
+            next.x = cur.x + dx[next.dir];
+
+            if(next.y < 0 || next.y > N || next.x < 0 || next.x > M || arr[next.y][next.x] != 0){
+                continue;
+            }
+            q.push(next);
+            break;
+        }
+        if(q.empty()){
+            ROBOT next;
+            next.dir = cur.dir;
+            next.y = cur.y + dy[(cur.dir+2) % 4];
+            next.x = cur.x + dx[(cur.dir+2) % 4];
+
+            if(arr[next.y][next.x] == 1){
+                break;
+            }
+            q.push(next);
+        }
+    }
+    
+    printf("%d\n", answer);
+    return 0;
+}
